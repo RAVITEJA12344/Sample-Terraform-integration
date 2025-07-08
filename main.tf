bb232c480b1983e8e6bda7f3ad5212ecfd9bea47
RAVITEJA12344 <67087488+RAVITEJA12344@users.noreply.github.com>
2025-07-08 15:14:36 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-000ec6c25978d5999"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "JenkinsTerraformEC2"
+  }
+}
